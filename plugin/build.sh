7b2b04e9ca193577f53afd91189755d57842a741
jinguihua <156711203@qq.com>
2023-12-06 17:59:21 +0800

    changed

--- plugin/build.sh
+++ plugin/build.sh
@@ -3,4 +3,4 @@
 #go build --buildmode=plugin -o /Users/luisjin/Downloads/json_to_model.so main.go
 
 #下面命令 可以生成 .h .so
-go build --buildmode=c-shared -o /Users/luisjin/Downloads/json_to_model.so main.go
+go build --buildmode=c-shared -o ../convert_gui/library/json_to_model.so main.go
